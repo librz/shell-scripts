059405876dbfeea30eaa2154f159984839fa68b5
rz <rz19960718@gmail.com>
2021-01-16 22:26:43 +0800

    using quite mode in curl and apt

--- show_non_printable.sh
+++ show_non_printable.sh
@@ -1,12 +1,12 @@
 # 起因: line feed 和 tab 都是不可见字符，而 cat -v -t -e 输出的结果不是 "\n" 和 "\t" 这种标准形式，开始自己造轮子
 
-# 普通用法: bash <(curl -L https://raw.githubusercontent.com/librz/shell_scripts/main/show_non_printable.sh) filename.txt
-# piping用法: echo "H" | bash <(curl -L https://raw.githubusercontent.com/librz/shell_scripts/main/show_non_printable)
+# 普通用法: bash <(curl -sL https://raw.githubusercontent.com/librz/shell_scripts/main/show_non_printable.sh) filename.txt
+# piping用法: echo "H" | bash <(curl -sL https://raw.githubusercontent.com/librz/shell_scripts/main/show_non_printable)
 
 # 思路：先把字符串转为 hex，替换 line feed 的 hex 为 \和n 的 hex, 替换 tab 的 hex 为 \和t 的 hex; 再用 xxd 把 hex 转为 ascii 字符
 
 # to use hexdump, bsdmainutils must be installed
-yes Y | apt install bsdmainutils xxd
+yes Y | apt -q install bsdmainutils xxd
 
 # newline ascii hex code: 0a
 # tab ascii hex code: 09
