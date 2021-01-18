3fbd6aff77b5ca07817e387c393f3dc3bfcf8234
rz <rz19960718@gmail.com>
2021-01-18 18:04:03 +0800

    Update flai.sh

--- flai.sh
+++ flai.sh
@@ -1,10 +1,8 @@
 #!/usr/bin
 
 # flai => find line and insert
-# usage: bash <(curl -L https://raw.githubusercontent.com/librz/shell_scripts/main/flai.sh) /path/to/file "first line\nsecond line"
+# usage: bash <(curl -L https://raw.githubusercontent.com/librz/shell_scripts/main/flai.sh) /path/to/file "text to search"  "first line\nsecond line"
 
-input=$1
-insertion=$2
 result=""
 found=false
 
@@ -13,15 +11,15 @@ do
   if [[ $found = true ]]; then
     result="$result\n$line"
   else 
-    echo "$line" | grep upgrade > /dev/null 2>&1 
+    echo "$line" | grep "$2" > /dev/null 2>&1 
     if [[ $? -eq 0 ]]; then
-      result="$result\n$line\n$insertion"
+      result="$result\n$line\n$3"
       found=true
     else
       result="$result\n$line"
     fi  
   fi  
-done < "$input"
+done < "$1"
 
 if [[ $found = false ]]; then
   echo "not found"
