895cb21bbf1564270f1397c593db050b44463e57
rz <rz19960718@gmail.com>
2021-01-20 22:57:19 +0800

    Update v2ray_install.sh

--- v2ray_install.sh
+++ v2ray_install.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# V2ray + tls + nginx + websocket 的安装和设置
+echo "V2ray + tls + nginx + websocket 安装配置脚本"
 
 echo "在运行此脚本之前，以下条件应该被满足："
-echo "1. 系统是 Ubuntu 18.04 或者以上"
+echo "1. 系统是 Ubuntu 18.04 或者以上并且使用 root 账号登陆"
 echo "2. 系统上没有安装过 certbot"
 echo "3. 已经准备好域名并且 DNS 的 A 记录已经将域名指向本机的IP地址"
 
-echo -n "是否继续? (Y/N) "
-read answer
+read -p "是否继续? (Y/N) " -r answer
 if [[ $answer != "Y" ]] && [[ $answer != "y" ]]; then
+	echo "已退出"
 	exit 1
 fi
 
 # get domain
-echo -n "Please input your domain: "
-read domain
+read -p "请输入域名: " -r domain
 
 # install curl, nginx, snapd and certbot
 yes Y | apt install curl nginx snapd
 snap install --classic certbot
 if [[ ! -f /usr/bin/certbot ]]; then
-				ln -s /snap/bin/certbot /usr/bin/certbot
+	ln -s /snap/bin/certbot /usr/bin/certbot
 fi
 
 # run scripts from https://github.com/v2fly/fhs-install-v2ray to install v2ray core and geo file
@@ -29,9 +28,10 @@ bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh) 
 
 # write v2ray config 
-config='{
+cat > /usr/local/etc/v2ray/config.json << "EOF"
+{
   "inbounds": [{
-    "port": 60000, 
+    "port": 60000,
     "protocol": "vmess",
     "settings": {
       "clients": [{ "id": "4cce13cd-770e-4b5b-8013-c16b6e62bbcc" }]
@@ -47,20 +47,34 @@ config='{
     "protocol": "freedom",
     "settings": {}
   }]
-}'
-echo $config > /usr/local/etc/v2ray/config.json
+}
+EOF
+
 service v2ray restart
 
 # hook certbot with nginx
-echo -e "Y\n$doamin\n" | certbot --nginx  --register-unsafely-without-email
-if [[ $? -ne 0 ]]; then
+if ! (echo -e "Y\n$domain\n" | certbot --nginx  --register-unsafely-without-email); then
 	echo "certbot 设置出现问题，安装失败"
 	exit 1
 fi
 
-# insert reverse proxy setting to nginx config
-setting='\nlocation /v2 {\n    proxy_redirect off;\n    proxy_pass http://127.0.0.1:60000; \n    proxy_http_version 1.1;\n    proxy_set_header Upgrade $http_upgrade;\n    proxy_set_header Connection "upgrade";\n    proxy_set_header Host $http_host;\n    proxy_set_header X-Real-IP $remote_addr;\n    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n  }\n'
-# find the fist occurence of "managed by Certbot" ans insert setting after it 
+# nginx reverse proxy setting 
+setting=$(
+cat << "EOF"
+location /v2 {
+	proxy_redirect off;
+	proxy_pass http://127.0.0.1:60000; 
+	proxy_http_version 1.1;
+	proxy_set_header Upgrade $http_upgrade;
+	proxy_set_header Connection "upgrade";
+	proxy_set_header Host $http_host;
+	proxy_set_header X-Real-IP $remote_addr;
+	proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+}
+EOF
+)
+
+# find the fist occurence of "managed by Certbot" and insert setting after it 
 bash <(curl -L https://raw.githubusercontent.com/librz/shell_scripts/main/flai.sh) /etc/nginx/sites-available/default "managed by Certbot"  "$setting"
 
 service nginx restart
