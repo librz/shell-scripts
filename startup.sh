ebdc57f422b1a59c8bfed543a63e0116bb2520a9
rz <rz19960718@gmail.com>
2021-01-20 23:03:32 +0800

    add dnsutils to apt install list

--- startup.sh
+++ startup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # install common softwares
-apt update && yes Y | apt install zsh vim snapd curl tldr tree net-tools language-pack-zh-hans
-snap install gost
+apt update && yes Y | apt install zsh vim snapd curl tldr tree net-tools dnsutils language-pack-zh-hans
 
 # chang shell to zsh
 chsh -s "$(which zsh)" && echo "switched login shell to zsh, you may need to re-login for this to take effect"
+
+# set timezone to Asia/Shanghai
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+echo "timzone is set to Asia/Shanghai"
 
 # custom vim config
 curl -L https://raw.githubusercontent.com/librz/shell_scripts/main/.vimrc > ~/.vimrc
@@ -17,9 +20,6 @@ echo "custom .vimrc and .zshrc is in place"
 # change ssh listen port(sshd port) to 9000, set ClientAliveInterval to 5 seconds
 sed -i 's/#Port 22/Port 9000/g' /etc/ssh/sshd_config
 sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 5/g' /etc/ssh/sshd_config
-service sshd restart
 echo "ssh listen port is set to 9000, ClientAliveInterval is set to 5"
-
-# set timezone to Asia/Shanghai
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-echo "timzone is set to Asia/Shanghai"
+echo "restarting sshd, you'll be disconnected if you are using ssh, wait for a minute and relogin"
+service sshd restart
