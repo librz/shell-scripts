0fe60d238017b4f15af6459314bb3abb7a31e376
rz <rz19960718@gmail.com>
2021-03-31 06:11:15 +0800

    update gsync.sh

--- gsync.sh
+++ gsync.sh
@@ -16,7 +16,7 @@ if [[ -n "$1" ]]; then
 fi
 
 if ! git fetch; then
-	exit 1
+	exit 3
 fi
 
 status=$(git status)
@@ -24,27 +24,27 @@ status=$(git status)
 if [[ "$status" =~ ahead ]]; then
 	echo "** Pushing local changes to origin/main **"
 	if ! git push; then
-		exit 2
+		exit 4
 	fi
 elif [[ "$status" =~ behind ]]; then
 	echo "** Pulling remote changes from origin/main **"
 	if ! git merge origin/"$branch" --ff-only; then
-		exit 3
+		exit 5
 	fi
 elif [[ "$status" =~ diverged ]]; then
 	if git merge origin/"$branch" --no-edit; then
 		echo "** Pushing changes to origin/main after successful merge **"
 		if ! git push; then
-			exit 2
+			exit 4
 		fi
 	else
 		echo "There seems to be merge conflicts, please resolve them mannually"
-		exit 3
+		exit 6
 	fi
 elif [[ "$status" =~ 'up to date' ]]; then
 	echo "You branch is up to date with origin/$branch"
 	exit 0
 else
 	echo "An unknown error occured, check the script you are running"
-	exit 4
+	exit 7
 fi
