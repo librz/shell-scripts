a6830b54c24a7be1b6ed8c3cc1f0e789812395c2
rz <rz19960718@gmail.com>
2021-04-02 11:48:49 +0800

    gsync.sh: improvements

--- gsync.sh
+++ gsync.sh
@@ -3,16 +3,30 @@
 # gsync: git sync
 # aim: bidirectional sync between local and remote git repo
 
+# usage: 
+# 1. sync current branch: bash gsync.sh 
+# 2. sync specific branch: bash gsync.sh {branch name}
+# e.g. bash gsync.sh dev
+
+# note: this script will peform auto merge when local & remote branch are diverged
+# rebase is not supported yet, maybe I'll add itlater
+
 if ! branch=$(git branch --show-current); then
+	# one of the reason you may come to this is:
+	# current folder is not even monitored by git
 	exit 1
 fi
 
+# if parameter is provided 
 if [[ -n "$1" ]]; then
 	if [[ ! $(git branch --list) =~ $1 ]]; then
-		echo "branch $1 doesn't exit"
+		echo "branch $1 doesn't exist"
 		exit 2
 	fi
-	branch="$1"
+	if [[ "$branch" != "$1" ]]; then
+		branch="$1"
+		git switch "$1"
+	fi
 fi
 
 if ! git fetch; then
@@ -32,6 +46,7 @@ elif [[ "$status" =~ behind ]]; then
 		exit 5
 	fi
 elif [[ "$status" =~ diverged ]]; then
+	# try to perform auto merge
 	if git merge origin/"$branch" --no-edit; then
 		echo "** Pushing changes to origin/main after successful merge **"
 		if ! git push; then
