bc41ed72016b48780765033fe07b186f0b0e852e
rz <rz19960718@gmail.com>
2021-04-05 23:52:48 +0800

    simplify output messages in init.sh

--- init.sh
+++ init.sh
@@ -10,55 +10,82 @@ function err {
 	echo "$1" >&2
 }
 
+function header {
+	echo "** $1 **"
+}
+
 # check distro
+header "checking distro"
 if ! distro=$(bash <(curl -sL http://realrz.com/shell-scripts/distro.sh)); then
 	exit 1
 fi
 echo "You are running: $distro"
 
-# install common softwares
+echo
+
+# install softwares
+header "installing common softwares"
 if [[ "$distro" == "Debian" || "$distro" == "Ubuntu" ]]; then
-	apt update
-	if yes | apt install zsh vim git snapd curl tldr tree xxd net-tools nmap dnsutils; then
-		echo "apt install succeeded"
+	if apt update &>/dev/null && yes | apt install zsh vim git snapd curl tldr tree xxd net-tools nmap dnsutils; then
+		echo "success"
 	else
 		err "apt install failed"
 		exit 2
 	fi
-else
-	# distro is macOS
-	echo "use homebrew to manually install softwares on macOS"
+elif [[ "$distro" == "macOS" ]]; then
+	echo "please use homebrew to manually install softwares on macOS"
+elif [[ "$distro" == "Windows" ]]; then
+	echo "please use scoop to manually install softwares on Windows"
 fi
+
+echo
 
 # language-pack-zh-hans is only aviailable in ubuntu 
 if [[ "$distro" = "Ubuntu" ]]; then
+	header "installing language-pack-zh-hans"
 	# also install language-pack-zh-hans
 	yes | apt install language-pack-zh-hans
+	echo "success"
 fi
 
 # add zsh to /etc/shells if it's not there
-if ! (grep zsh /etc/shells &>/dev/null); then
+if ! grep -q zsh /etc/shells; then
 	command -v zsh >> /etc/shells
 fi
+
 # change login shell to zsh
-if chsh -s "$(command -v zsh)"; then
-	echo "login shell is changed to zsh"
-else
-	err "failed to change login shell to zsh"
-	exit 3
+if [[ "$distro" != "Windows" ]]; then
+	header "Changing login shell to zsh"
+	if chsh -s "$(command -v zsh)"; then
+		echo "login shell is changed to zsh"
+	else
+		err "failed to change login shell to zsh"
+		exit 3
+	fi
 fi
 
-#set timezone to Asia/Shanghai
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+echo
 
-# install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-				http://realrz.com/shell-scripts/plug.vim
+header "setting timezone to Asia/Shanghai"
+if ! diff /usr/share/zoneinfo/Asia/Shanghai /etc/localtime; then
+	cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+fi
+echo "success"
 
-# custom vim config
-curl -L http://realrz.com/shell-scripts/.vimrc > ~/.vimrc
-# custom zsh config
-curl -L http://realrz.com/shell-scripts/.zshrc > ~/.zshrc
+echo
+
+header "installing vim-plug"
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs http://realrz.com/shell-scripts/plug.vim
+echo "success"
+
+echo
+
+header "configuring .vimrc & .zshrc"
+curl -sL http://realrz.com/shell-scripts/.vimrc > ~/.vimrc
+curl -sL http://realrz.com/shell-scripts/.zshrc > ~/.zshrc
+echo "success"
+
+echo
 
 if [[ "$distro" != "macOS" ]]; then
 	# change ssh listen port(sshd port) to 9000, set ClientAliveInterval to 5 seconds
@@ -68,4 +95,6 @@ if [[ "$distro" != "macOS" ]]; then
 	service sshd restart
 fi
 
-echo "some of these changes may require re-login to be effective"
+echo
+
+echo "system config finished, some changes may require re-login to be effective"
